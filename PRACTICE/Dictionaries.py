1d6660cdc188e7073c1a35348a88a6ac89e84ae9
Theresa Karamor <teriekarie@gmail.com>
2022-08-22 18:51:05 +0100

    Adding multiple keys

--- PRACTICE/Dictionaries.py
+++ PRACTICE/Dictionaries.py
@@ -2,3 +2,14 @@
 menue = {"avocado toast": 6, "carrot juice": 5, "bleberry muffin": 2}
 
 print(type(menue))
+
+# Ading a key
+menue["cheesecake"] = 7
+
+# Adding the new key ti the dictionary
+menue = {"avocado toast": 6, "carrot juice": 5, "bleberry muffin": 2, "cheesecake": 7}
+
+# Adding multiple keys
+menue.update({"banana": 6, "lemonade": 5, "parfait": 4})
+
+print(menue)
