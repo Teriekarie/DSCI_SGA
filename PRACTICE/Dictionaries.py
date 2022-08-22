c8d6bfd3293362fcdd4896fa90877994c47678f9
Theresa Karamor <teriekarie@gmail.com>
2022-08-22 22:41:42 +0100

    more on dictionaries

--- PRACTICE/Dictionaries.py
+++ PRACTICE/Dictionaries.py
@@ -21,3 +21,30 @@ age = [30, 26, 27, 35, 25]
 students = {key:value for key, value in zip(names, age)}
 print(students)
 
+# calling out an element from the dictionary
+print(menue["banana"])
+print(students["Bola"])
+
+# Using the get() method
+print(menue.get("lemonade"))
+
+# Removing elements from a dictionary
+print(students.pop("Fay"))
+
+# Randomly removes an item 
+print(students.popitem())
+
+# Removing all item from dictionary
+students.clear()
+print(students)
+
+# creating a dictionary with key value pairs as num:squares
+# numbers with range of 6
+Box = {}
+for x in range(8):
+    Box[x] = x*x
+    print(Box)
+
+Box = {x: x*x for x in range(8)}
+print(Box)
+
