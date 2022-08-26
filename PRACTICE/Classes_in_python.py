a12604d48af106201934f62bdd9a607f79a9db69
Theresa Karamor <teriekarie@gmail.com>
2022-08-26 16:18:30 +0100

    classes

---
+++ PRACTICE/Classes_in_python.py
@@ -0,0 +1,15 @@
+# create a class 
+class Family:
+    pass
+Father = Family()
+mother = Family()
+
+Father.name = 'Jacob'
+Father.age = 49
+Father.occupation = 'Doctor'
+
+mother.name = 'Maria'
+mother.age = 35
+mother.occupation = 'Lawyer'
+
+print(mother.age)
