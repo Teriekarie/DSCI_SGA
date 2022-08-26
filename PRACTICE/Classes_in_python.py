e72ed95d41e833eec6d459ade8943d05ea6c3ddf
Theresa Karamor <teriekarie@gmail.com>
2022-08-26 16:48:20 +0100

    creating a parent_bio function

--- PRACTICE/Classes_in_python.py
+++ PRACTICE/Classes_in_python.py
@@ -4,11 +4,20 @@ class Family:
         self.name = name
         self.age = age
         self.occupation = occupation
+        
+    def parent_bio(self):
+        return '{} {} {}'.format(self.name, self.age, self.occupation)
+    
 
+# defining the objects attributes
 Father = Family('Jacob', 49, 'Doctor')
 mother = Family('Maria', 35, 'Lawyer')
 
 print(Father.occupation)
+
+print(mother.parent_bio())
+# merging a part of each object together with the {} place holders
+print('{} {}'.format(Father.occupation, mother.age))
 
 # Father.name = 'Jacob'
 # Father.age = 49
